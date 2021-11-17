aa24839f1ea6f5ae42418d8b6d6d8401fd3d5bac
NurMuhammadSyaifuddin <nurm9301@gmail.com>
2021-11-17 12:01:33 +0700

    add onBackPressed

---
+++ app/src/main/java/com/project/fakerepo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.project.fakerepo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
